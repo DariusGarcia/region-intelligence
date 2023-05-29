85a44ae40aa974cb6d855bdbf1efd0b37df0d7f6
Darius Garcia <dariusgarcia888@gmail.com>
2023-05-29 13:46:10 -0700

    updated the single project page

---
+++ components/cards/leftAlignedCard.jsx
@@ -0,0 +1,123 @@
+import capitalizeWords from '@/utils/capitalizeWords'
+import { PaperClipIcon } from '@heroicons/react/20/solid'
+
+export default function LeftAlignedCard({ data }) {
+  return (
+    <div className='overflow-hidden bg-white shadow sm:rounded-lg'>
+      {data &&
+        data.map((item) => (
+          <>
+            <div className='flex flex-col px-4 py-6 sm:px-6 w-full'>
+              <h3 className='text-md font-semibold leading-7 text-blue-600 mb-4'>
+                Project Information
+              </h3>
+              <p className=' text-3xl font-medium mb-4 mt-1 max-w-2xl  leading-6  text-gray-900'>
+                {item.caseNumbers}
+              </p>
+              <p className=' text-xl font-medium mb-4 mt-1 max-w-2xl  leading-6  text-gray-900'>
+                {capitalizeWords(item.listingNames)}
+              </p>
+            </div>
+            <div className='border-t border-gray-100 w-full'>
+              <dl className='divide-y divide-gray-100'>
+                <div className='flex flex-col px-4 py-6  sm:gap-4 sm:px-6 w-96 lg:w-[600px]'>
+                  {item.imageUrls === 'None' ? (
+                    <img
+                      src='https://i0.wp.com/theperfectroundgolf.com/wp-content/uploads/2022/04/placeholder.png?w=1200&ssl=1'
+                      alt='image'
+                      className='rounded-md shadow-md'
+                    />
+                  ) : (
+                    <img
+                      alt='project'
+                      className='h-full w-full shadow-md rounded-md'
+                      src={item.imageUrls}
+                    />
+                  )}
+                  <p className='mt-4 italic'>
+                    Last update: {item.recentUpdate}
+                  </p>
+                  <dt className='text-md mt-8 font-medium text-gray-900'>
+                    Project address
+                  </dt>
+                  <dd className='mt-1 text-md leading-6 text-gray-700 sm:col-span-2 sm:mt-0'>
+                    {item.projectLocations}, {item.city}, CA
+                  </dd>
+                </div>
+                <div className='px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
+                  <dt className='text-md font-medium text-gray-900'>
+                    Applicant
+                  </dt>
+                  <dd className='mt-1 text-md leading-6 text-gray-700 sm:col-span-2 sm:mt-0'>
+                    {capitalizeWords(item.applicant)}
+                  </dd>
+                </div>
+                <div className='px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
+                  <dt className='text-md font-medium text-gray-900'>
+                    Applicant email address
+                  </dt>
+                  <dd className='mt-1 text-md leading-6 text-gray-700 sm:col-span-2 sm:mt-0'>
+                    {item.applicantEmail}
+                  </dd>
+                </div>
+                <div className='px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
+                  <dt className='text-md font-medium text-gray-900'>
+                    Project manager
+                  </dt>
+                  <dd className='mt-1 text-md leading-6 text-gray-700 sm:col-span-2 sm:mt-0'>
+                    {item.plannerName === null || item.plannerName == ''
+                      ? 'Undisclosed'
+                      : item.plannerName}
+                  </dd>
+                </div>
+                <div className='px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
+                  <dt className='text-md font-medium text-gray-900'>
+                    Planner's phone number
+                  </dt>
+                  <dd className='mt-1 text-md leading-6 text-gray-700 sm:col-span-2 sm:mt-0'>
+                    {item.plannerPhone}
+                  </dd>
+                </div>
+                <div className='px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
+                  <dt className='text-md font-medium leading-6 text-gray-900'>
+                    Planner's email address
+                  </dt>
+                  <dd className='mt-1 text-md leading-6 text-gray-700 sm:col-span-2 sm:mt-0'>
+                    {item.plannerEmail}
+                  </dd>
+                </div>
+                <div className='px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
+                  <dt className='text-md font-medium leading-6 text-gray-900'>
+                    Type of use
+                  </dt>
+                  <dd className='mt-1 text-md leading-6 text-gray-700 sm:col-span-2 sm:mt-0'>
+                    {item.typeOfUse}
+                  </dd>
+                </div>
+                <div className='px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
+                  <dt className='text-md font-medium leading-6 text-gray-900'>
+                    Project description
+                  </dt>
+                  <dd className='mt-1 text-md leading-6 text-gray-700 sm:col-span-2 sm:mt-0'>
+                    <p className='list-disc max-w-lg text-left'>
+                      {item.projectDescriptions}
+                    </p>
+                  </dd>
+                </div>
+                <div className='px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
+                  <dt className='text-md font-medium leading-6 text-gray-900'>
+                    Project status
+                  </dt>
+                  <dd className='mt-1 text-md leading-6 text-gray-700 sm:col-span-2 sm:mt-0'>
+                    <p className='list-disc max-w-lg text-left'>
+                      {item.projectStatus}
+                    </p>
+                  </dd>
+                </div>
+              </dl>
+            </div>
+          </>
+        ))}
+    </div>
+  )
+}
--- pages/land-directory/list-view/[id].jsx
+++ pages/land-directory/list-view/[id].jsx
@@ -6,6 +6,7 @@ import Link from 'next/link'
 import { createClient } from '@supabase/supabase-js'
 import { BounceLoader } from 'react-spinners'
 import capitalizeWords from '@/utils/capitalizeWords'
+import LeftAlignedCard from '@/components/cards/leftAlignedCard'
 
 export default function ListViewPage() {
   const supabase = createClient(
@@ -33,13 +34,8 @@ export default function ListViewPage() {
     fetchPermits()
   }, [id])
 
-  console.log({ cityProjects: cityProject })
-
-  function replaceHyphensWithSpaces(str) {
-    return str.replace(/-/g, ' ')
-  }
   return (
-    <div className='flex justify-center mt-12 px-4'>
+    <div className='flex justify-center pt-12 px-4 bg-stone-50 pb-8'>
       <div className='max-w-7xl'>
         {loading ? (
           <div className='flex justify-center my-24'>
@@ -47,82 +43,8 @@ export default function ListViewPage() {
           </div>
         ) : (
           <>
-            {cityProject &&
-              cityProject.map((item) => (
-                <section
-                  className='flex flex-col justify-center w-full gap-4'
-                  key={item.caseNumbers}
-                >
-                  <h1 className='text-3xl font-medium mb-4'>
-                    {item.caseNumbers}
-                  </h1>
-                  {item.imageUrls === 'None' ? (
-                    <Image
-                      src={image}
-                      width={500}
-                      height={500}
-                      alt='image'
-                      className='rounded-md shadow-lg'
-                    />
-                  ) : (
-                    <img
-                      alt='project'
-                      className='h-full w-full shadow-md rounded-md'
-                      src={item.imageUrls}
-                    />
-                  )}
-                  <p className='italic'>Last update: {item.recentUpdate}</p>
-                  <p className='text-xl font-medium'>
-                    Project address: {item.projectLocations}
-                  </p>
-                  <p className='text-lg'>
-                    <span className='font-medium'> Applicant: </span>
-                    {capitalizeWords(item.applicant)}
-                  </p>
-                  <p className='text-lg'>
-                    <span className='font-medium'> Applicant's email: </span>
-                    {item.applicantEmail}
-                  </p>
-                  <p className='text-lg'>
-                    <span className='font-medium'> Project manager: </span>
-                    {item.plannerName === null || item.plannerName === ''
-                      ? 'Undisclosed'
-                      : item.plannerName}
-                  </p>
-                  <p className='text-lg'>
-                    <span className='font-medium'> Planner's Phone: </span>
-                    {item.plannerPhone}
-                  </p>
-                  <p className='text-lg'>
-                    <span className='font-medium'> Planner's Email: </span>
-                    {item.plannerEmail}
-                  </p>
-                  <p className='text-lg'>
-                    <span className='font-medium'> Listing name: </span>
-                    {item.listingNames}
-                  </p>
-                  <p className='text-lg'>
-                    <span className='font-medium'> Type of use: </span>
-                    {item.typeOfUse}
-                  </p>
-                  <p className='text-lg'>
-                    <span className='font-medium'> City: </span>
-                    {item.city}
-                  </p>
-
-                  <ul className='text-lg'>
-                    <span className='font-medium'> Project Description: </span>
-                    <li className='list-disc ml-8 max-w-lg text-left'>
-                      {item.projectDescriptions}
-                    </li>
-                  </ul>
-                  <p className='text-lg'>
-                    <span className='font-medium'> Project Status: </span>
-                    {item.projectStatus}
-                  </p>
-                </section>
-              ))}
-            <div className='mt-24 flex justify-center'>
+            {cityProject && <LeftAlignedCard data={cityProject} />}
+            <div className='my-12  flex justify-center'>
               <Link
                 href='/land-directory'
                 className='bg-blue-600 hover:bg-blue-500 transition ease-out text-white p-2 rounded-md'
