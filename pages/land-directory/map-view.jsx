97f8ba7c69cd83a7bd576b50519a0c52a0da46cc
Darius Garcia <dariusgarcia888@gmail.com>
2023-05-31 17:01:28 -0700

    add marker data for all permits

--- pages/land-directory/map-view.jsx
+++ pages/land-directory/map-view.jsx
@@ -17,24 +17,23 @@ import formatAddressForGeocoding from '@/utils/formatAddressForGeocoding'
 
 export default function MapsPage() {
   const session = useSession()
-  const { isLoaded } = useLoadScript({
-    googleMapsApiKey: process.env.NEXT_PUBLIC_MAP_API_KEY,
-  })
-
   useEffect(() => {
     const timeout = setTimeout(() => {
       if (!session) {
         Router.push('/login')
       }
-    }, 100) // Delay of 0.1 seconds (100 milliseconds)
-    return () => clearTimeout(timeout) // Cleanup the timeout on component unmount
+    }, 100) // 0.1 seconds (100 milliseconds)
+    return () => clearTimeout(timeout)
   }, [session])
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.NEXT_PUBLIC_MAP_API_KEY,
+  })
 
   const [selectedMarker, setSelectedMarker] = useState(null)
   const [isSlideOverOpen, setIsSlideOverOpen] = useState(false)
   const [selectedMarkerData, setSelectedMarkerData] = useState(null)
-  const [permitData, setPermitData] = useState({})
-  const [permitCoord, setPermitCoord] = useState({})
+  const [permitData, setPermitData] = useState([])
+  const [permitCoord, setPermitCoord] = useState([])
   const [isLoading, setLoading] = useState(false)
   const [error, setError] = useState(null)
 
@@ -46,41 +45,65 @@ export default function MapsPage() {
     const { data, error } = await supabase.from('cityProjects').select()
     if (error) {
       setError(error)
-      setLoading(false)
       return
     }
-    if (data) {
-      setPermitData(data)
-      setLoading(false)
-    }
+
+    setPermitData(data)
+    getCoordinates(data)
   }
+
+  // async function getCoordinates(permitData) {
+  //   const permitDataWithGeolocation = []
+
+  //   permitData?.map(async (permit) => {
+  //     const { projectLocations } = permit
+  //     const response = await fetch(
+  //       `https://maps.googleapis.com/maps/api/geocode/json?address=${formatAddressForGeocoding(
+  //         projectLocations
+  //       )},+${permit.city},+CA&key=${process.env.NEXT_PUBLIC_GEOCODE_API_KEY}`
+  //     )
+
+  //     const data = await response.json()
+  //     const geolocation = data.results[0]?.geometry?.location
+
+  //     permitDataWithGeolocation.push({
+  //       ...permit,
+  //       lat: geolocation?.lat,
+  //       lng: geolocation?.lng,
+  //     })
+  //   })
+
+  //   setPermitCoord(permitDataWithGeolocation)
+  //   console.log({ permitCoord: permitCoord })
+  // }
 
   async function getCoordinates(permitData) {
-    const permitDataWithGeolocation = []
+    const permitDataWithGeolocation = await Promise.all(
+      permitData.map(async (permit) => {
+        const { projectLocations } = permit
+        const response = await fetch(
+          `https://maps.googleapis.com/maps/api/geocode/json?address=${formatAddressForGeocoding(
+            projectLocations
+          )},+${permit.city},+CA&key=${process.env.NEXT_PUBLIC_GEOCODE_API_KEY}`
+        )
 
-    permitData?.map(async (permit) => {
-      const { projectLocations } = permit
-      const response = await fetch(
-        `https://maps.googleapis.com/maps/api/geocode/json?address=${formatAddressForGeocoding(
-          projectLocations
-        )},+${permit.city},+CA&key=${process.env.NEXT_PUBLIC_GEOCODE_API_KEY}`
-      )
+        const data = await response.json()
+        const geolocation = data.results[0]?.geometry?.location
 
-      const data = await response.json()
-      const geolocation = data.results[0]?.geometry?.location
-
-      permitDataWithGeolocation.push({ ...permit, geolocation })
-      // permitDataWithGeolocation.push({ ...permit, projectLocations })
-    })
+        return {
+          ...permit,
+          lat: geolocation?.lat,
+          lng: geolocation?.lng,
+        }
+      })
+    )
 
     setPermitCoord(permitDataWithGeolocation)
+    console.log({ permitCoord: permitCoord })
   }
-
-  console.log({ updatedPermitData: permitCoord })
   useEffect(() => {
     setLoading(true)
     fetchPermits()
-    getCoordinates(permitData)
     setLoading(false)
   }, [])
 
@@ -104,101 +127,119 @@ export default function MapsPage() {
   const renderMap = () => {
     return (
       <>
-        <Head>
-          <title>First Property - Maps</title>
-        </Head>
-        <div className='w-full flex flex-col justify-center mt-8 md:mt-12 px-2 md:px-8'>
-          <h1 className='flex justify-center font-bold text-2xl mb-8'>
-            Pending permits locations
-          </h1>
-          <GoogleMap
-            center={{ lat: 33.611263, lng: -117.91887 }}
-            zoom={12}
-            options={{ mapTypeId: 'hybrid' }}
-            mapContainerStyle={{
-              margin: '20px 0 0',
-              height: '70vh',
-              width: '100%',
-              borderRadius: '8px',
-            }}
-          >
-            {randomCoord.map(
-              ([
-                name,
-                lat,
-                lng,
-                ownerName,
-                phoneNumber,
-                lotSize,
-                projectStatus,
-                lastUpdate,
-                listingName,
-                location,
-                description,
-              ]) => (
-                <Marker
-                  key={name}
-                  position={{ lat, lng }}
-                  onClick={() =>
-                    handleMarkerClick({
-                      name,
-                      lat,
-                      lng,
-                      ownerName,
-                      phoneNumber,
-                      lotSize,
-                      projectStatus,
-                      lastUpdate,
-                      listingName,
-                      location,
-                      description,
-                    })
-                  }
-                />
-              )
-            )}
-            {selectedMarker && (
-              <InfoWindow
-                position={{ lat: selectedMarker.lat, lng: selectedMarker.lng }}
-                onCloseClick={handleInfoWindowClose}
-                onClick={() =>
-                  handleMarkerClick({
-                    name,
-                    lat,
-                    lng,
-                    ownerName,
-                    phoneNumber,
-                    lotSize,
-                    projectStatus,
-                    lastUpdate,
-                    listingName,
-                    location,
-                    description,
-                  })
+        {permitCoord && !isLoading && (
+          <>
+            <Head>
+              <title>First Property - Maps</title>
+            </Head>
+            <div className='w-full flex flex-col justify-center mt-8 md:mt-12 px-2 md:px-8'>
+              <h1 className='flex justify-center font-bold text-2xl mb-8'>
+                Pending permits locations
+              </h1>
+              <GoogleMap
+                center={
+                  selectedMarker
+                    ? { lat: selectedMarker.lat, lng: selectedMarker.lng }
+                    : { lat: 33.8036758, lng: -117.9119473 }
                 }
+                zoom={13}
+                options={{ mapTypeId: 'hybrid' }}
+                mapContainerStyle={{
+                  margin: '20px 0 0',
+                  height: '70vh',
+                  width: '100%',
+                  borderRadius: '8px',
+                }}
               >
-                <article className='flex flex-col gap-2 pb-4 md:pb-0 md:pr-0 pr-2 w-full'>
-                  <p>{selectedMarker.name}</p>
-                  <p>{selectedMarker.ownerName}</p>
-                  <p>{selectedMarker.phoneNumber}</p>
-                  <p>Lot size: {selectedMarker.lotSize}</p>
-                  <p>Status: {selectedMarker.projectStatus}</p>
-                </article>
-              </InfoWindow>
-            )}
-          </GoogleMap>
-          <SlideOver
-            isOpen={isSlideOverOpen}
-            onClose={() => setIsSlideOverOpen(!isSlideOverOpen)}
-            markerData={selectedMarkerData}
-          />
-          <div className='mt-24 w-full '>
-            <PermitsTable />
-          </div>
-        </div>
+                {permitCoord?.map((permit) => (
+                  <Marker
+                    key={permit.id}
+                    position={{
+                      lat: Number(permit?.lat),
+                      lng: Number(permit?.lng),
+                    }}
+                    onClick={() =>
+                      handleMarkerClick({
+                        applicant: permit.applicant,
+                        applicantEmail: permit.applicantEmail,
+                        applicantPhone: permit.applicantPhone,
+                        caseNumbers: permit.caseNumbers,
+                        city: permit.city,
+                        created_at: permit.created_at,
+                        id: permit.id,
+                        imageUrls: permit.imageUrls,
+                        lat: permit.lat,
+                        listingNames: permit.listingNames,
+                        lng: permit.lng,
+                        plannerEmail: permit.plannerEmail,
+                        plannerName: permit.plannerName,
+                        plannerPhone: permit.plannerPhone,
+                        projectDescriptions: permit.projectDescriptions,
+                        projectLocations: permit.projectLocations,
+                        projectStatus: permit.projectStatus,
+                        recentUpdate: permit.recentUpdate,
+                        typeOfUse: permit.typeOfUse,
+                      })
+                    }
+                  />
+                ))}
+                {selectedMarker && (
+                  <InfoWindow
+                    position={{
+                      lat: selectedMarker.lat,
+                      lng: selectedMarker.lng,
+                    }}
+                    onCloseClick={handleInfoWindowClose}
+                    onClick={() =>
+                      handleMarkerClick({
+                        applicant,
+                        applicantEmail,
+                        applicantPhone,
+                        caseNumbers,
+                        city,
+                        created_at,
+                        id,
+                        imageUrls,
+                        lat,
+                        listingNames,
+                        lng,
+                        plannerEmail,
+                        plannerName,
+                        plannerPhone,
+                        projectDescriptions,
+                        projectLocations,
+                        projectStatus,
+                        recentUpdate,
+                        typeOfUse,
+                      })
+                    }
+                  >
+                    <article className='flex flex-col gap-2 pb-4 md:pb-0 md:pr-0 pr-2 w-full'>
+                      <p>{selectedMarker.caseNumbers}</p>
+                      <p>{selectedMarker.listingNames}</p>
+                      <p>Address: {selectedMarker.projectLocations}</p>
+                      <p>Applicant: {selectedMarker.applicant}</p>
+                      <p>Planner's name: {selectedMarker.plannerName}</p>
+                      <p>Planner's email: {selectedMarker.plannerEmail}</p>
+                      <p>Status: {selectedMarker.projectStatus}</p>
+                    </article>
+                  </InfoWindow>
+                )}
+              </GoogleMap>
+              <SlideOver
+                isOpen={isSlideOverOpen}
+                onClose={() => setIsSlideOverOpen(!isSlideOverOpen)}
+                markerData={selectedMarkerData}
+              />
+              <div className='mt-24 w-full '>
+                <PermitsTable />
+              </div>
+            </div>
+          </>
+        )}
       </>
     )
   }
 
-  return isLoaded ? renderMap() : null
+  return isLoaded && permitCoord && !isLoading ? renderMap() : null
 }
