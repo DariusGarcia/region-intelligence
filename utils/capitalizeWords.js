883e02db50735cc10de2233567972b64b0c871ec
Darius Garcia <dariusgarcia888@gmail.com>
2023-06-12 19:47:28 -0700

    add edge cases

--- utils/capitalizeWords.js
+++ utils/capitalizeWords.js
@@ -1,6 +1,16 @@
 export default function capitalizeWords(str) {
+  if (!str) {
+    return '' // Return an empty string if the input is undefined or empty
+  }
+
+  // Check if the string is a single word
+  if (!str.includes(' ')) {
+    // If it is, capitalize the entire word and return it
+    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase()
+  }
+
   // Split the string into an array of words
-  const words = str.toLowerCase().split(' ')
+  const words = str?.toLowerCase().split(' ')
 
   // Capitalize the first letter of each word
   const capitalizedWords = words.map((word) => {
