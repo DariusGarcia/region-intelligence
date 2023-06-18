bf7ad69cdaa54ea55d942d53f240752fe10f2091
Darius Garcia <dariusgarcia888@gmail.com>
2023-06-17 22:41:06 -0700

    update landing header image

--- components/header/landingHeader.jsx
+++ components/header/landingHeader.jsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import { motion as m, AnimatePresence } from 'framer-motion'
+import mobileImg from '../../public/mobile.png'
 
 export default function LandingHeader() {
   const cardVariants = {
@@ -20,24 +21,22 @@ export default function LandingHeader() {
   }
 
   return (
-    <div className='relative bg-white max-w-7xl'>
-      <div className='mx-auto max-w-7xl lg:grid lg:grid-cols-12 lg:gap-x-8 lg:px-8'>
+    <div className='relative bg-white '>
+      <div className='mx-auto flex flex-row lg:px-8'>
         <AnimatePresence>
           <m.div
             initial='hidden'
             animate='visible'
             viewport={{ once: true, amount: 0.8 }}
-            className='px-6  md:pt-10 sm:pb-32 lg:col-span-7 lg:px-0 lg:pb-56 lg:pt-48 xl:col-span-6'
-          >
+            className='px-6 sm:pb-32 lg:px-0   flex flex-col items-center gap-4 '>
             <m.div variants={cardVariants}>
-              <div className='mx-auto max-w-2xl lg:mx-0'>
-                <div className='hidden sm:mt-32 sm:flex lg:mt-16'>
+              <div className='flex flex-col justify-center mx-auto max-w-2xl lg:mx-0'>
+                <div className='hidden sm:mt-32 md:mt-0 sm:flex '>
                   <div className='relative rounded-full px-3 py-1 text-sm leading-6 text-gray-500 ring-1 ring-gray-900/10 hover:ring-gray-900/20'>
                     Announcing our next round of funding.{' '}
                     <Link
                       href='#'
-                      className='whitespace-nowrap font-semibold text-blue-600'
-                    >
+                      className='whitespace-nowrap font-semibold text-blue-600'>
                       <span className='absolute inset-0' aria-hidden='true' />
                       Read more <span aria-hidden='true'>&rarr;</span>
                     </Link>
@@ -57,26 +56,24 @@ export default function LandingHeader() {
                 <div className='mt-10 flex items-center gap-x-6'>
                   <Link
                     href='/pricing'
-                    className='rounded-md bg-blue-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600'
-                  >
+                    className='rounded-md bg-blue-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600'>
                     Get started
                   </Link>
                   <Link
                     href='/pricing'
-                    className='text-sm font-semibold leading-6 text-gray-900'
-                  >
+                    className='text-sm font-semibold leading-6 text-gray-900'>
                     Learn more <span aria-hidden='true'>→</span>
                   </Link>
                 </div>
               </div>
             </m.div>
-            <div className='relative mt-12 md:mt-0 lg:col-span-5 lg:-mr-8 xl:absolute xl:inset-0 xl:left-1/2 xl:mr-0'>
+            <div className='relative mt-12 md:mt-24 mb-24 md:mb-0'>
               <Image
-                className='aspect-[3/2] w-full rounded-lg shadow-xl bg-gray-50 object-cover lg:absolute lg:inset-0 lg:aspect-auto lg:h-full'
-                src='https://images.unsplash.com/photo-1498758536662-35b82cd15e29?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2102&q=80'
+                className=' w-full rounded-lg  bg-gray-50 lg:h-full'
+                src={mobileImg}
                 alt='landing'
-                height={500}
-                width={500}
+                height={1000}
+                width={1000}
                 priority
               />
             </div>
