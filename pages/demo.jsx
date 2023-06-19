ba5bba2920ddd1722271bb9d7f910f769285474f
Darius Garcia <dariusgarcia888@gmail.com>
2023-06-19 00:22:57 -0700

    added demo page

---
+++ pages/demo.jsx
@@ -0,0 +1,15 @@
+import Testimonials from '@/components/demo/demo'
+import React from 'react'
+
+const Demo = () => {
+  return (
+    <div className='md:mt-12 justify-center'>
+      <h1 className='text-2xl font-bold text-center'>
+        Here is a video walkthrough of how to use the map.
+      </h1>
+      <Testimonials />
+    </div>
+  )
+}
+
+export default Demo
