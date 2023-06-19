b2b8fb5067c01409bcb400be4aebe04dab0a4b31
Darius Garcia <dariusgarcia888@gmail.com>
2023-06-19 01:20:25 -0700

    change connection pooling

--- pages/api/data/ceqa.js
+++ pages/api/data/ceqa.js
@@ -8,27 +8,39 @@ const pool = mysql.createPool({
   connectionLimit: process.env.CONNECTION_LIMIT, // Adjust this value as per your requirements
 })
 
-export default async function handler(req, res) {
-  pool.getConnection((error, connection) => {
-    if (error) {
-      // Handle connection error
-      console.error('Error connecting to database:', error)
-      res.status(500).json({ error: 'Internal Server Error' })
-    } else {
-      const query = `SELECT * FROM ${process.env.CEQA_DB}`
-
-      connection.query(query, (error, results) => {
-        connection.release() // Release the connection back to the pool
-
-        if (error) {
-          // Handle query error
-          console.error('Error executing query:', error)
-          res.status(500).json({ error: 'Internal Server Error' })
-        } else {
-          // Process query results
-          res.json(results)
-        }
-      })
-    }
+// Create a reusable connection from the pool
+const getConnection = () => {
+  return new Promise((resolve, reject) => {
+    pool.getConnection((error, connection) => {
+      if (error) {
+        reject(error)
+      } else {
+        resolve(connection)
+      }
+    })
   })
+}
+
+export default async function handler(req, res) {
+  try {
+    const connection = await getConnection()
+    const query = `SELECT * FROM ${process.env.CEQA_DB}`
+
+    connection.query(query, (error, results) => {
+      connection.release() // Release the connection back to the pool
+
+      if (error) {
+        // Handle query error
+        console.error('Error executing query:', error)
+        res.status(500).json({ error: 'Internal Server Error' })
+      } else {
+        // Process query results
+        res.json(results)
+      }
+    })
+  } catch (error) {
+    // Handle connection error
+    console.error('Error connecting to database:', error)
+    res.status(500).json({ error: 'Internal Server Error' })
+  }
 }
