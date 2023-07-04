f45b69436b0d0a9f5af45cc06ce99af40e5be734
Darius Garcia <dariusgarcia888@gmail.com>
2023-07-03 17:22:27 -0700

    updated pricing features

--- components/cta.jsx
+++ components/cta.jsx
@@ -11,7 +11,7 @@ export default function Cta() {
         </h2>
         <div className='mt-10 flex items-center gap-x-6 lg:mt-0 lg:flex-shrink-0'>
           <Link
-            href='/pricing'
+            href='/signup'
             className='rounded-md bg-blue-600 px-3.5 py-2.5 w-72 text-center text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600'>
             Get started
           </Link>
