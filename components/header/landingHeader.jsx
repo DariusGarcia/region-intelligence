edf464d40c86bcc5042134f92543a80e2a055af4
Darius Garcia <dariusgarcia888@gmail.com>
2023-07-04 13:22:18 -0700

    wdith issues

--- components/header/landingHeader.jsx
+++ components/header/landingHeader.jsx
@@ -4,8 +4,8 @@ import { motion as m, AnimatePresence } from 'framer-motion'
 
 export default function LandingHeader({ scrollToTarget }) {
   return (
-    <div className='relative bg-white max-w-[100vw] w-full'>
-      <div className='mx-auto flex flex-wrap lg:px-8 w-full'>
+    <div className='relative bg-white max-w-[100vw] '>
+      <div className='mx-auto flex flex-wrap lg:px-8'>
         <AnimatePresence>
           <m.div
             initial='hidden'
