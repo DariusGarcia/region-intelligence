96bfbd472d5a992903919e7ce9c415391f985655
Darius Garcia <dariusgarcia888@gmail.com>
2023-07-06 13:18:03 -0700

    change radio group to select menus

--- features/auth/signUpQuestions.jsx
+++ features/auth/signUpQuestions.jsx
@@ -20,6 +20,7 @@ export default function Questions({
 
   function handlePrimaryPurposeChange(event) {
     const value = event.target.value
+    console.log({ years: value })
     onPrimaryPurposeChange(value)
   }
 
@@ -33,111 +34,68 @@ export default function Questions({
           <p className='mt-1 text-sm leading-6 text-gray-600'>
             Please answer these questions so that we can get to know you better.
           </p>
-          {/* industry role radio selection */}
+          {/* industry role select menu */}
           <div className='mt-10 '>
-            <fieldset>
-              <legend className='text-base font-semibold text-gray-900'>
+            <div>
+              <label
+                htmlFor='industry'
+                className='block text-sm font-medium leading-6 text-gray-900'>
                 What is your role in the real estate industry? (Required)
-              </legend>
-              <div className='mt-4 divide-y divide-gray-200 border-b border-t border-gray-200'>
+              </label>
+              <select
+                id='industry'
+                name='industry'
+                className='mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-blue-600 sm:text-sm sm:leading-6'
+                onChange={handleIndustryRoleChange}
+                required>
                 {industryRole.map((industry, industryIdx) => (
-                  <div
-                    key={industryIdx}
-                    className='relative flex items-start py-4'>
-                    <div className='min-w-0 flex-1 text-sm leading-6'>
-                      <label
-                        htmlFor={`industry-${industry.id}`}
-                        className='select-none font-medium text-gray-900'>
-                        {industry.name}
-                      </label>
-                    </div>
-                    <div className='ml-3 flex h-6 items-center'>
-                      <input
-                        id={`industry-${industry.id}`}
-                        name='industry role'
-                        type='radio'
-                        required
-                        value={industry.name}
-                        onChange={handleIndustryRoleChange}
-                        defaultChecked={industry.id === null}
-                        className='h-4 w-4 border-gray-300 text-blue-600 focus:ring-blue-600'
-                      />
-                    </div>
-                  </div>
+                  <option key={industryIdx} value={industry.name}>
+                    {industry.name}
+                  </option>
                 ))}
-              </div>
-            </fieldset>
+              </select>
+            </div>
             {/* years of experience radio group */}
             <div className='my-8'>
-              <fieldset>
-                <legend className='text-base font-semibold text-gray-900'>
-                  How many years of experience do you have in the real estate
-                  industry? (Required)
-                </legend>
-                <div className='mt-4 divide-y divide-gray-200 border-b border-t border-gray-200'>
-                  {yearsExpOptions.map((experience, experienceIdx) => (
-                    <div
-                      key={experienceIdx}
-                      className='relative flex items-start py-4'>
-                      <div className='min-w-0 flex-1 text-sm leading-6'>
-                        <label
-                          htmlFor={`experience-${experience.id}`}
-                          className='select-none font-medium text-gray-900'>
-                          {experience.name}
-                        </label>
-                      </div>
-                      <div className='ml-3 flex h-6 items-center'>
-                        <input
-                          id={`experience-${experience.id}`}
-                          name='years of experience'
-                          type='radio'
-                          required
-                          value={experience.name}
-                          onChange={handleYearsOfExperienceChange}
-                          defaultChecked={experience.id === null}
-                          className='h-4 w-4 border-gray-300 text-blue-600 focus:ring-blue-600'
-                        />
-                      </div>
-                    </div>
-                  ))}
-                </div>
-              </fieldset>
+              <label
+                htmlFor='experience'
+                className='block text-sm font-medium leading-6 text-gray-900'>
+                How many years of experience do you have in the real estate
+                industry? (Required)
+              </label>
+              <select
+                id='experience'
+                name='experience'
+                className='mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-blue-600 sm:text-sm sm:leading-6'
+                onChange={handleYearsOfExperienceChange}
+                required>
+                {yearsExpOptions.map((experience, experienceIdx) => (
+                  <option key={experienceIdx} value={experience.name}>
+                    {experience.name}
+                  </option>
+                ))}
+              </select>
             </div>
             {/* primary purpose radio group */}
             <div className='my-8'>
-              <fieldset>
-                <legend className='text-base font-semibold text-gray-900'>
-                  What is your primary purpose you will be using our tool for?
-                  (Required)
-                </legend>
-                <div className='mt-4 divide-y divide-gray-200 border-b border-t border-gray-200'>
-                  {mainPurposeOptions.map((purpose, purposeIdx) => (
-                    <div
-                      key={purposeIdx}
-                      className='relative flex items-start py-4'>
-                      <div className='min-w-0 flex-1 text-sm leading-6'>
-                        <label
-                          htmlFor={`purpose-${purpose.id}`}
-                          className='select-none font-medium text-gray-900'>
-                          {purpose.name}
-                        </label>
-                      </div>
-                      <div className='ml-3 flex h-6 items-center'>
-                        <input
-                          id={`purpose-${purpose.id}`}
-                          name='primary purpose'
-                          type='radio'
-                          required
-                          value={purpose.name}
-                          onChange={handlePrimaryPurposeChange}
-                          defaultChecked={purpose.id === null}
-                          className='h-4 w-4 border-gray-300 text-blue-600 focus:ring-blue-600'
-                        />
-                      </div>
-                    </div>
-                  ))}
-                </div>
-              </fieldset>
+              <label
+                htmlFor='purpose'
+                className='block text-sm font-medium leading-6 text-gray-900'>
+                What is your primary purpose you will be using our tool for?
+                (Required)
+              </label>
+              <select
+                id='purpose'
+                name='purpose'
+                className='mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-blue-600 sm:text-sm sm:leading-6'
+                onChange={handlePrimaryPurposeChange}
+                required>
+                {mainPurposeOptions.map((purpose, purposeIdx) => (
+                  <option key={purposeIdx} value={purpose.name}>
+                    {purpose.name}
+                  </option>
+                ))}
+              </select>
             </div>
             {/* challenges looking to overcome textarea */}
             <div className='w-full my-4'>
@@ -152,7 +110,7 @@ export default function Questions({
                   id='challenges'
                   name='challenges'
                   onChange={(e) => onChallengesOvercomeChange(e.target.value)}
-                  className='block w-full rounded-md border-0 py-1.5 h-12 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-0 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6'
+                  className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6'
                   defaultValue={''}
                 />
               </div>
@@ -170,7 +128,7 @@ export default function Questions({
                   id='challenges'
                   name='other tools'
                   onChange={(e) => onOtherToolsChange(e.target.value)}
-                  className='block w-full rounded-md border-0 py-1.5 h-12 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-0 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6'
+                  className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6'
                   defaultValue={''}
                 />
               </div>
@@ -188,7 +146,7 @@ export default function Questions({
                   id='challenges'
                   name='idea tool'
                   onChange={(e) => onIdealToolChange(e.target.value)}
-                  className='block w-full rounded-md border-0 py-1.5 h-12 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-0 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6'
+                  className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6'
                   defaultValue={''}
                 />
               </div>
@@ -199,13 +157,13 @@ export default function Questions({
                 <label
                   htmlFor='city'
                   className='block text-sm font-medium leading-6 text-gray-900'>
-                  What city are you based in? (Required)
+                  What city are you based in? (Optional)
                 </label>
                 <div className='mt-2 flex items-center gap-x-3'>
                   <input
                     id='city'
                     name='city'
-                    className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-0 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6'
+                    className='block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6'
                     defaultValue={''}
                     onChange={(e) => onCityChange(e.target.value)}
                   />
@@ -214,13 +172,14 @@ export default function Questions({
               {/* how user found us input field */}
               <div className=''>
                 <label
-                  htmlFor='photo'
+                  htmlFor='find-out'
                   name='foundUs'
                   className='block text-sm font-medium leading-6 text-gray-900'>
                   How did you find out about us? (Optional)
                 </label>
                 <div className='mt-2 flex items-center gap-x-3'>
                   <input
+                    name='find-out'
                     type='text'
                     className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6'
                     onChange={(e) => onFoundUsChange(e.target.value)}
@@ -259,6 +218,7 @@ export default function Questions({
 }
 
 const industryRole = [
+  { id: 0, name: 'Please select' },
   { id: 1, name: 'Real Estate Agent' },
   { id: 2, name: 'Broker' },
   { id: 3, name: 'Property Manger' },
@@ -267,6 +227,7 @@ const industryRole = [
 ]
 
 const mainPurposeOptions = [
+  { id: 0, name: 'Please select' },
   { id: 1, name: 'Parcel information / Property research' },
   { id: 2, name: 'City planning listings' },
   { id: 3, name: 'Market trends analysis' },
@@ -274,6 +235,7 @@ const mainPurposeOptions = [
 ]
 
 const yearsExpOptions = [
+  { id: 0, name: 'Please select' },
   { id: 1, name: 'Less than 1 year' },
   { id: 2, name: '1-3 years' },
   { id: 3, name: '4-6 years' },
