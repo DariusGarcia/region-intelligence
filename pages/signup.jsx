9524daa4930a1baf815aeecab32092ca522638e3
Darius Garcia <dariusgarcia888@gmail.com>
2023-07-06 13:45:32 -0700

    add sign up btn disabled when not all fields are filled out

--- pages/signup.jsx
+++ pages/signup.jsx
@@ -241,8 +241,19 @@ export default function SignupPage() {
               <button
                 type='submit'
                 onClick={handleSignUp}
-                disabled={loading ? true : false}
-                className='flex w-full justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600'>
+                disabled={
+                  industryRole === '' ||
+                  yearsExperience === '' ||
+                  primaryPurpose === '' ||
+                  loading
+                }
+                className={
+                  industryRole === '' ||
+                  yearsExperience === '' ||
+                  primaryPurpose === ''
+                    ? 'flex w-full justify-center rounded-md bg-blue-900 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600'
+                    : 'flex w-full justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600'
+                }>
                 Sign up
               </button>
             </div>
