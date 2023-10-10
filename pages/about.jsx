e25c2851c6db703a44d1ee94d49a890d7d319af0
Darius Garcia <dariusgarcia888@gmail.com>
2023-10-10 14:07:24 -0700

    added more sections

--- pages/about.jsx
+++ pages/about.jsx
@@ -2,6 +2,11 @@ import { useState } from 'react'
 import Image from 'next/image'
 import { Dialog } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+import {
+  CloudArrowUpIcon,
+  LockClosedIcon,
+  ServerIcon,
+} from '@heroicons/react/20/solid'
 
 export default function AboutPage() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
@@ -35,7 +40,7 @@ export default function AboutPage() {
                 width={800}
                 height={800}
                 alt='aerial view of LA'
-                className='aspect-[6/5] w-full max-w-lg rounded-2xl object-cover sm:mt-16 lg:mt-0 lg:max-w-none xl:row-span-2 xl:row-end-2'
+                className='aspect-[6/5] w-full max-w-lg my-8 md:my-0 rounded-2xl object-cover sm:mt-16 lg:mt-0 lg:max-w-none xl:row-span-2 xl:row-end-2'
               />
             </div>
           </div>
@@ -74,7 +79,7 @@ export default function AboutPage() {
         </div>
 
         {/* Logo cloud */}
-        <div className='mx-auto mt-32 max-w-7xl sm:mt-40 sm:px-6 lg:px-8'>
+        {/* <div className='mx-auto mt-32 max-w-7xl sm:mt-40 sm:px-6 lg:px-8'>
           <div className='relative isolate overflow-hidden bg-gray-900 px-6 py-24 text-center shadow-2xl sm:rounded-3xl sm:px-16'>
             <h2 className='mx-auto max-w-2xl text-3xl font-bold tracking-tight text-white sm:text-4xl'>
               Our customers love us
@@ -133,66 +138,185 @@ export default function AboutPage() {
               />
             </div>
           </div>
-        </div>
+        </div> */}
 
         {/* Content section */}
-        <div className='mt-32 overflow-hidden sm:mt-40'>
-          <div className='mx-auto max-w-7xl px-6 lg:flex lg:px-8'>
-            <div className='mx-auto grid max-w-2xl grid-cols-1 gap-x-12 gap-y-16 lg:mx-0 lg:min-w-full lg:max-w-none lg:flex-none lg:gap-y-8'>
-              <div className='lg:col-end-1 lg:w-full lg:max-w-lg lg:pb-8'>
-                <h2 className='text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl'>
-                  Our people
-                </h2>
-                <p className='mt-6 text-xl leading-8 text-gray-600'>
-                  Quasi est quaerat. Sit molestiae et. Provident ad dolorem
-                  occaecati eos iste. Soluta rerum quidem minus ut molestiae
-                  velit error quod. Excepturi quidem expedita molestias quas.
-                </p>
-                <p className='mt-6 text-base leading-7 text-gray-600'>
-                  Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure
-                  qui lorem cupidatat commodo. Elit sunt amet fugiat veniam
-                  occaecat fugiat. Quasi aperiam sit non sit neque
-                  reprehenderit.
-                </p>
-              </div>
-              <div className='flex flex-wrap items-start justify-end gap-6 sm:gap-8 lg:contents'>
-                <div className='w-0 flex-auto lg:ml-auto lg:w-auto lg:flex-none lg:self-end'>
-                  <img
-                    src='https://images.unsplash.com/photo-1670272502246-768d249768ca?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1152&q=80'
-                    alt=''
-                    className='aspect-[7/5] w-[37rem] max-w-none rounded-2xl bg-gray-50 object-cover'
+        <div className='bg-white py-12 md:py-32 md:mt-24'>
+          <div className='mx-auto grid max-w-7xl grid-cols-1 gap-x-8 gap-y-20 px-6 lg:px-8 xl:grid-cols-5'>
+            <div className='max-w-2xl xl:col-span-2'>
+              <h2 className='text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl'>
+                Meet the Visionaries
+              </h2>
+              <p className='mt-6 text-lg leading-8 text-gray-600'>
+                We’re a dynamic group of individuals who are passionate about
+                what we do and dedicated to delivering the best results for our
+                clients.
+              </p>
+            </div>
+            <ul
+              role='list'
+              className='-mt-12 space-y-12 divide-y divide-gray-200 xl:col-span-3'>
+              {people.map((person) => (
+                <li
+                  key={person.name}
+                  className='flex flex-col gap-10 pt-12 sm:flex-row'>
+                  <Image
+                    className='aspect-[4/5] w-52 flex-none rounded-2xl object-cover'
+                    src={person.imageUrl}
+                    alt='Portrait of founders'
+                    width={900}
+                    height={900}
                   />
+                  <div className='max-w-xl flex-auto'>
+                    <h3 className='text-lg font-semibold leading-8 tracking-tight text-gray-900'>
+                      {person.name}
+                    </h3>
+                    <p className='text-base leading-7 text-gray-600'>
+                      {person.role}
+                    </p>
+                    <p className='mt-6 text-base leading-7 text-gray-600'>
+                      {person.bio}
+                    </p>
+                    <ul role='list' className='mt-6 flex gap-x-6'>
+                      {/* <li>
+                        <a
+                          href={person.twitterUrl}
+                          className='text-gray-400 hover:text-gray-500'>
+                          <span className='sr-only'>Twitter</span>
+                          <svg
+                            className='h-5 w-5'
+                            aria-hidden='true'
+                            fill='currentColor'
+                            viewBox='0 0 20 20'>
+                            <path d='M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84' />
+                          </svg>
+                        </a>
+                      </li> */}
+                      <li>
+                        <a
+                          href={person.linkedinUrl}
+                          className='text-gray-400 hover:text-gray-500'>
+                          <span className='sr-only'>LinkedIn</span>
+                          <svg
+                            className='h-5 w-5'
+                            aria-hidden='true'
+                            fill='currentColor'
+                            viewBox='0 0 20 20'>
+                            <path
+                              fillRule='evenodd'
+                              d='M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z'
+                              clipRule='evenodd'
+                            />
+                          </svg>
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+
+        {/* Construction image */}
+        {/* <div className='py-24 md:py-32'>
+          <div className='flex justify-center mx-auto max-w-7xl px-6 lg:px-8'>
+            <Image
+              src='/about/construction1.jpg'
+              alt='construction'
+              width={900}
+              height={900}
+              className='rounded-md shadow-md'
+            />
+          </div>
+        </div> */}
+
+        {/* Feature sections */}
+        <div className='overflow-hidden bg-gray-900 py-16 sm:py-32'>
+          <div className='mx-auto max-w-7xl px-6 lg:px-8'>
+            <div className='mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2'>
+              <div className='lg:pr-8 lg:pt-4'>
+                <div className='lg:max-w-lg'>
+                  <h2 className='text-base font-semibold leading-7 text-blue-500'>
+                    Platform Insights
+                  </h2>
+                  <p className='mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl'>
+                    Our Data in Action
+                  </p>
+                  <p className='mt-6 text-lg leading-8 text-gray-300'>
+                    ArcGIS: Visual Representation of the Esri Project in Action
+                  </p>
+                  <dl className='mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-300 lg:max-w-none'>
+                    {features.map((feature) => (
+                      <div key={feature.name} className='relative pl-9'>
+                        <dt className='inline font-semibold text-white'>
+                          <feature.icon
+                            className='absolute left-1 top-1 h-5 w-5 text-blue-500'
+                            aria-hidden='true'
+                          />
+                          {feature.name}
+                        </dt>{' '}
+                        <dd className='inline'>{feature.description}</dd>
+                      </div>
+                    ))}
+                  </dl>
                 </div>
-                <div className='contents lg:col-span-2 lg:col-end-2 lg:ml-auto lg:flex lg:w-[37rem] lg:items-start lg:justify-end lg:gap-x-8'>
-                  <div className='order-first flex w-64 flex-none justify-end self-end lg:w-auto'>
-                    <img
-                      src='https://images.unsplash.com/photo-1605656816944-971cd5c1407f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=768&h=604&q=80'
-                      alt=''
-                      className='aspect-[4/3] w-[24rem] max-w-none flex-none rounded-2xl bg-gray-50 object-cover'
-                    />
-                  </div>
-                  <div className='flex w-96 flex-auto justify-end lg:w-auto lg:flex-none'>
-                    <img
-                      src='https://images.unsplash.com/photo-1568992687947-868a62a9f521?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1152&h=842&q=80'
-                      alt=''
-                      className='aspect-[7/5] w-[37rem] max-w-none flex-none rounded-2xl bg-gray-50 object-cover'
-                    />
-                  </div>
-                  <div className='hidden sm:block sm:w-0 sm:flex-auto lg:w-auto lg:flex-none'>
-                    <img
-                      src='https://images.unsplash.com/photo-1612872087720-bb876e2e67d1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=768&h=604&q=80'
-                      alt=''
-                      className='aspect-[4/3] w-[24rem] max-w-none rounded-2xl bg-gray-50 object-cover'
-                    />
-                  </div>
+              </div>
+              <Image
+                src='/about/arcGISDemo.png'
+                alt='Product screenshot'
+                className='w-full max-w-none rounded-xl shadow-md ring-1 ring-white/10 sm:w-[57rem] md:-ml-4 lg:-ml-0'
+                width={2432}
+                height={1442}
+              />
+            </div>
+          </div>
+        </div>
+        <div className='overflow-hidden bg-gray-900 py-16 sm:py-32'>
+          <div className='mx-auto max-w-7xl px-6 lg:px-8'>
+            <div className='mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2'>
+              <div className='lg:ml-auto lg:pl-4 lg:pt-4'>
+                <div className='lg:max-w-lg'>
+                  <h2 className='text-base font-semibold leading-7 text-blue-500'>
+                    Instant support
+                  </h2>
+                  <p className='mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl'>
+                    Dev
+                  </p>
+                  <p className='mt-6 text-lg leading-8 text-white'>
+                    The AI that will be your knowledgeable best friend when it
+                    comes to planning and development projects.
+                  </p>
+                  <dl className='mt-10 max-w-xl space-y-8 text-base leading-7 text-white lg:max-w-none'>
+                    {features2.map((feature) => (
+                      <div key={feature.name} className='relative pl-9'>
+                        <dt className='inline font-semibold text-white'>
+                          <feature.icon
+                            className='absolute left-1 top-1 h-5 w-5 text-blue-500'
+                            aria-hidden='true'
+                          />
+                          {feature.name}
+                        </dt>{' '}
+                        <dd className='inline'>{feature.description}</dd>
+                      </div>
+                    ))}
+                  </dl>
                 </div>
+              </div>
+              <div className='flex items-start justify-end lg:order-first'>
+                <Image
+                  src='/about/Dev.jpg'
+                  alt='Product screenshot'
+                  className='w-full max-w-none rounded-md shadow-lg ring-1 ring-gray-400/10 sm:w-[40rem]'
+                  width={900}
+                  height={900}
+                />
               </div>
             </div>
           </div>
         </div>
-
         {/* Stats */}
-        <div className='mx-auto mt-32 max-w-7xl px-6 sm:mt-40 lg:px-8'>
+        {/* <div className='mx-auto mt-12 max-w-7xl px-6 lg:px-8'>
           <div className='mx-auto max-w-2xl lg:mx-0'>
             <h2 className='text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl'>
               We approach the workplace as something that adds to our lives and
@@ -247,7 +371,7 @@ export default function AboutPage() {
               </div>
             </div>
           </div>
-        </div>
+        </div> */}
 
         {/* Content section */}
         <div className='mx-auto mt-32 max-w-7xl px-6 sm:mt-40 lg:px-8'>
@@ -357,6 +481,26 @@ const timeline = [
     dateTime: '2023-12',
   },
 ]
+
+const people = [
+  {
+    name: 'Julian Sotelo',
+    role: 'Founder',
+    imageUrl: '/about/julianPortrait.JPG',
+    bio: "Julian is a data science expert who received a Master's degree in Business Analytics from the University of California, Irvine. Coming from a family background in real-estate development, Julian is determined to provide a service that connects the information gap between bureaucratic organizations, the private sector, and the public.",
+    // twitterUrl: '#',
+    linkedinUrl: '#',
+  },
+  {
+    name: 'Darius Garcia Jr.',
+    role: 'Co-Founder',
+    imageUrl: '/about/portrait.jpg',
+    bio: "Darius is a Fullstack Developer who received his Business Economics Bachelor's degree from the University of California, Irvine. He also received a certificate for completing the Fullstack Web Developing bootcamp from UC Irvine as well. Darius currently works as a Coding Instructor, as well as a freelance web developer.",
+    // twitterUrl: '#',
+    linkedinUrl: '#',
+  },
+]
+
 const jobOpenings = [
   {
     id: 1,
@@ -375,5 +519,29 @@ const jobOpenings = [
       'Et veniam et officia dolorum rerum. Et voluptas consequatur magni sapiente amet voluptates dolorum. Ut porro aut eveniet.',
     salary: '$125,000 USD',
     location: 'San Francisco, CA',
+  },
+]
+
+const features = [
+  {
+    name: 'Planning Development Data.',
+    description:
+      'Keep Up to Date with current approved and pre-approved planning developments across SoCal. ',
+    icon: CloudArrowUpIcon,
+  },
+  {
+    name: 'Vital parcel information.',
+    description:
+      'Track vital parcel information such as zoning, hazard areas, acreage, and more! ',
+    icon: ServerIcon,
+  },
+]
+
+const features2 = [
+  {
+    name: 'AI Chatbot.',
+    description:
+      'Dev is the code and regulation expert when it comes to building and real-estate development, feel free to ask Dev any question!',
+    icon: CloudArrowUpIcon,
   },
 ]
