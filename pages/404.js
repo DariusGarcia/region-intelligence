840f8f0b65b0b09f7ddc527f0d53d40f30b576aa
Darius Garcia <dariusgarcia888@gmail.com>
2024-02-20 13:03:57 -0800

    update 404 page

--- pages/404.js
+++ pages/404.js
@@ -6,75 +6,78 @@ import {
   QuestionMarkCircleIcon,
   RssIcon,
 } from '@heroicons/react/24/solid'
+import Footer from '@/components/navbar/footer'
+import Navbar from '@/components/navbar/navbar'
 
 export default function ErrorPage() {
   return (
-    <div className='bg-white'>
-      <main className='mx-auto w-full max-w-7xl px-6 pb-16 pt-10 lg:px-8'>
-        <h1 className='w-full text-center font-bold text-4xl'>
-          Region Intelligence
-        </h1>
-        <div className='mx-auto mt-20 max-w-2xl text-center sm:mt-24'>
-          <p className='text-2xl font-semibold leading-8 text-blue-600'>404</p>
-          <h1 className='mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl'>
-            This page does not exist
-          </h1>
-          <p className='mt-4 text-base leading-7 text-gray-600 sm:mt-6 sm:text-lg sm:leading-8'>
-            Sorry, we couldn’t find the page you’re looking for.
-          </p>
-        </div>
-        <div className='mx-auto mt-16 flow-root max-w-lg sm:mt-20'>
-          <h2 className='sr-only'>Popular pages</h2>
-          <ul
-            role='list'
-            className='-mt-6 divide-y divide-gray-900/5 border-b border-gray-900/5'>
-            {links.map((link, linkIdx) => (
-              <li
-                key={linkIdx}
-                className='relative flex gap-x-6 py-6 hover:bg-stone-50 transition ease-out p-2 rounded-md'>
-                <div className='flex h-10 w-10 flex-none items-center justify-center rounded-lg shadow-sm ring-1 ring-gray-900/10'>
-                  <link.icon
-                    className='h-6 w-6 text-blue-600'
-                    aria-hidden='true'
-                  />
-                </div>
-                <div className='flex-auto'>
-                  <h3 className='text-sm font-semibold leading-6 text-gray-900'>
-                    <Link href={link.href}>
-                      <span className='absolute inset-0' aria-hidden='true' />
-                      {link.name}
-                    </Link>
-                  </h3>
-                  <p className='mt-2 text-sm leading-6 text-gray-600'>
-                    {link.description}
-                  </p>
-                </div>
-                <div className='flex-none self-center'>
-                  <ChevronRightIcon
-                    className='h-5 w-5 text-gray-400'
-                    aria-hidden='true'
-                  />
-                </div>
-              </li>
-            ))}
-          </ul>
-          <div className='mt-10 flex justify-center'>
-            <Link
-              href='/'
-              className='text-sm font-semibold leading-6 text-blue-600 hover:text-blue-500 transition ease-out'>
-              <span aria-hidden='true'>&larr;</span>
-              Back to home
-            </Link>
+    <>
+      <Navbar />
+      <div className='bg-white'>
+        <main className='mx-auto w-full max-w-7xl px-6 pb-16 pt-10 lg:px-8'>
+          <div className='mx-auto max-w-2xl text-center'>
+            <p className='text-2xl font-semibold leading-8 text-blue-600'>
+              404
+            </p>
+            <h1 className='mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl'>
+              This page does not exist
+            </h1>
+            <p className='mt-4 text-base leading-7 text-gray-600 sm:mt-6 sm:text-lg sm:leading-8'>
+              Sorry, we couldn’t find the page you’re looking for.
+            </p>
           </div>
-        </div>
-      </main>
-      <footer className='border-t border-gray-100 py-6 sm:py-10'>
-        <div className='mx-auto flex max-w-7xl flex-col items-center justify-center gap-8 px-6 sm:flex-row lg:px-8'>
-          <p className='text-sm leading-7 text-gray-400'>
-            &copy; Region Intelligence, Inc. All rights reserved.
-          </p>
-          <div className='hidden sm:block sm:h-7 sm:w-px sm:flex-none sm:bg-gray-200' />
-          {/* <div className='flex gap-x-4'>
+          <div className='mx-auto mt-16 flow-root max-w-lg sm:mt-20'>
+            <h2 className='sr-only'>Popular pages</h2>
+            <ul
+              role='list'
+              className='-mt-6 divide-y divide-gray-900/5 border-b border-gray-900/5'>
+              {links.map((link, linkIdx) => (
+                <li
+                  key={linkIdx}
+                  className='relative flex gap-x-6 py-6 hover:bg-stone-50 transition ease-out p-2 rounded-md'>
+                  <div className='flex h-10 w-10 flex-none items-center justify-center rounded-lg shadow-sm ring-1 ring-gray-900/10'>
+                    <link.icon
+                      className='h-6 w-6 text-blue-600'
+                      aria-hidden='true'
+                    />
+                  </div>
+                  <div className='flex-auto'>
+                    <h3 className='text-sm font-semibold leading-6 text-gray-900'>
+                      <Link href={link.href}>
+                        <span className='absolute inset-0' aria-hidden='true' />
+                        {link.name}
+                      </Link>
+                    </h3>
+                    <p className='mt-2 text-sm leading-6 text-gray-600'>
+                      {link.description}
+                    </p>
+                  </div>
+                  <div className='flex-none self-center'>
+                    <ChevronRightIcon
+                      className='h-5 w-5 text-gray-400'
+                      aria-hidden='true'
+                    />
+                  </div>
+                </li>
+              ))}
+            </ul>
+            <div className='mt-10 flex justify-center'>
+              <Link
+                href='/'
+                className='text-sm font-semibold leading-6 text-blue-600 hover:text-blue-500 transition ease-out'>
+                <span aria-hidden='true'>&larr;</span>
+                Back to home
+              </Link>
+            </div>
+          </div>
+        </main>
+        <footer className='border-t border-gray-100 py-6 sm:py-10'>
+          <div className='mx-auto flex max-w-7xl flex-col items-center justify-center gap-8 px-6 sm:flex-row lg:px-8'>
+            <p className='text-sm leading-7 text-gray-400'>
+              &copy; Region Intelligence, Inc. All rights reserved.
+            </p>
+            <div className='hidden sm:block sm:h-7 sm:w-px sm:flex-none sm:bg-gray-200' />
+            {/* <div className='flex gap-x-4'>
             {social.map((item, itemIdx) => (
               <a
                 key={itemIdx}
@@ -85,9 +88,11 @@ export default function ErrorPage() {
               </a>
             ))}
           </div> */}
-        </div>
-      </footer>
-    </div>
+          </div>
+        </footer>
+      </div>
+      <Footer />
+    </>
   )
 }
 
