e5b7df1e6a9194593c686524aaf04b643cb174cc
Darius Garcia <dariusgarcia888@gmail.com>
2024-02-23 21:02:25 -0800

    fix error

--- features/auth/personalSettingsProfile.jsx
+++ features/auth/personalSettingsProfile.jsx
@@ -98,7 +98,6 @@ export default function PersonalSettingsProfile() {
       alert(error.message)
       displayError()
     } else {
-      setSuccess('Profile updated successfully')
       displaySuccess()
     }
     setLoading(false)
