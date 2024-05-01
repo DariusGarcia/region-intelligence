3520893e8c068c93054ce4680afaf2f625791a92
Darius Garcia <dariusgarcia888@gmail.com>
2024-04-30 22:03:05 -0700

    update about us page

--- pages/about.jsx
+++ pages/about.jsx
@@ -35,20 +35,19 @@ export default function AboutPage({ posts }) {
           today!'
         />
       </Head>
-      <div className='bg-white'>
+      <div className='bg-[#FDFBF5]'>
         <main className='isolate'>
           {/* Hero section */}
-          <div className='relative isolate -z-10 overflow-hidden bg-gradient-to-b from-blue-100/20 '>
-            <div
-              className='absolute inset-y-0 right-1/2 -z-10 -mr-96 w-[200%] origin-top-right skew-x-[-30deg] bg-white shadow-xl shadow-blue-600/10 ring-1 ring-blue-50 sm:-mr-80 lg:-mr-96'
-              aria-hidden='true'
-            />
+          <div className='relative isolate -z-10 overflow-hidden bg-gradient-to-b from-blue-100/20 pt-24 '>
             <div className='mx-auto max-w-7xl px-6 py-4 sm:py-12 lg:px-8 flex '>
               <div className='mx-auto w-full lg:mx-0  lg:max-w-none  flex flex-col md:flex-row gap-12 justify-between  items-center'>
-                <div className='mt-6 max-w-3xl lg:mt-0 flex flex-col gap-8 justify-center w-full'>
-                  <h1 className='max-w-4xl text-3xl md:text-6xl font-bold tracking-tight text-gray-900 lg:col-span-2 xl:col-auto uppercase'>
-                    Region Intelligence
-                  </h1>
+                <div className='mt-6 max-w-3xl lg:mt-0 flex flex-col items-center gap-8 justify-center w-full'>
+                  <div className='flex flex-row items-center'>
+                    <img src='/simpleLogo.png' alt='logo' className='w-72' />
+                    <h1 className='max-w-4xl text-3xl md:text-6xl font-bold tracking-tight text-gray-900 lg:col-span-2 xl:col-auto uppercase'>
+                      RI
+                    </h1>
+                  </div>
                   <p className='pl-1 text-lg leading-8 text-gray-600 uppercase font-semibold'>
                     Based in Los Angeles
                   </p>
@@ -59,21 +58,16 @@ export default function AboutPage({ posts }) {
                   width={1080}
                   height={1280}
                   alt='aerial view of LA'
-                  className='aspect-[6/5] w-full max-w-lg my-8 md:my-0 rounded-2xl object-cover sm:mt-16 lg:mt-0 xl:row-span-2 xl:row-end-2'
+                  className='aspect-[7/4] w-full max-w-xl my-8 md:my-0 rounded-md object-cover sm:mt-16 lg:mt-0 xl:row-span-2 xl:row-end-2'
                 />
               </div>
             </div>
-            <div className='absolute inset-x-0 bottom-0 -z-10 h-24 bg-gradient-to-t from-white sm:h-32' />
           </div>
 
           {/* Principles section */}
-          <div className='w-full flex justify-center bg-gray-900 text-white my-12 '>
+          <div className='w-full flex justify-center bg-neutral-800 text-white my-12 '>
             <div className='max-w-7xl px-6'>
-              <section className='flex flex-col justify-center gap-4 py-12'>
-                <ul className='list-disc ml-4'>
-                  {' '}
-                  <li className='text-orange-500 font-semibold'>The Mission</li>
-                </ul>
+              <section className='flex flex-col justify-center gap-4 py-16'>
                 {mission.map((item) => (
                   <article key={item.title} className='py-4'>
                     <p className='text-3xl uppercase font-semibold'>
@@ -82,6 +76,9 @@ export default function AboutPage({ posts }) {
                     <div className='flex flex-col gap-8 mt-6'>
                       <p>{item.body1}</p>
                       <p>{item.body2}</p>
+                      <p>{item.body3}</p>
+                      <p>{item.body4}</p>
+                      <p>{item.body5}</p>
                     </div>
                   </article>
                 ))}
@@ -90,13 +87,13 @@ export default function AboutPage({ posts }) {
           </div>
           {/* Timeline section */}
           <div className='mx-auto max-w-7xl px-6 lg:px-8 mt-12 md:mt-24 border-b pb-24'>
-            <h2 className='mb-8 text-3xl font-bold'>Our Timeline</h2>
+            <h2 className='mb-8 text-3xl font-bold '>COMPANY TIMELINE</h2>
             <div className='mx-auto grid max-w-2xl grid-cols-1 gap-8 overflow-hidden lg:mx-0 lg:max-w-none lg:grid-cols-3'>
               {timeline.map((item) => (
                 <div key={item.name}>
                   <time
                     dateTime={item.dateTime}
-                    className='flex items-center text-sm font-semibold leading-6 text-blue-600'>
+                    className='flex items-center text-sm font-semibold leading-6'>
                     <svg
                       viewBox='0 0 4 4'
                       className='mr-4 h-1 w-1 flex-none'
@@ -119,114 +116,6 @@ export default function AboutPage({ posts }) {
               ))}
             </div>
           </div>
-
-          {/* Feature sections */}
-          {/* <FeatureSection /> */}
-
-          {/* Mission and values */}
-          {/* <div className='bg-white'>
-            <div className='mx-auto max-w-2xl px-4 py-24 sm:px-6 lg:max-w-7xl lg:px-8'>
-              <div className='mx-auto max-w-3xl text-center'>
-                <h2 className='text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl'>
-                  Our Mission and Values
-                </h2>
-                <p className='mt-4 text-gray-500'>
-                  Building transparent urban futures.
-                </p>
-              </div>
-
-              <div className='mt-16 space-y-16'>
-                {values.map((value, valueIdx) => (
-                  <div
-                    key={value.name}
-                    className='flex flex-col-reverse lg:grid lg:grid-cols-12 lg:items-center lg:gap-x-8'>
-                    <div
-                      className={classNames(
-                        valueIdx % 2 === 0
-                          ? 'lg:col-start-1'
-                          : 'lg:col-start-8 xl:col-start-9',
-                        'mt-6 lg:col-span-5 lg:row-start-1 lg:mt-0 xl:col-span-4'
-                      )}>
-                      <h3 className='text-lg font-medium text-gray-900'>
-                        {value.name}
-                      </h3>
-                      <p className='mt-2 text-md text-gray-500'>
-                        {value.description}
-                      </p>
-                    </div>
-                    <div
-                      className={classNames(
-                        valueIdx % 2 === 0
-                          ? 'lg:col-start-6 xl:col-start-5'
-                          : 'lg:col-start-1',
-                        'flex-auto lg:col-span-7 lg:row-start-1 xl:col-span-8'
-                      )}>
-                      <div className='aspect-h-2 aspect-w-5 overflow-hidden rounded-lg bg-gray-100'>
-                        <Image
-                          src={value.imageSrc}
-                          alt={value.imageAlt}
-                          height={1280}
-                          width={1080}
-                          className='object-cover object-center'
-                        />
-                      </div>
-                    </div>
-                  </div>
-                ))}
-              </div>
-            </div>
-          </div> */}
-
-          {/* CTA section */}
-          {/* <div className='bg-blue-700 mt-12'>
-            <div className='px-6 py-24 sm:px-6 sm:py-32 lg:px-8'>
-              <div className='mx-auto max-w-2xl text-center'>
-                <h2 className='text-3xl font-bold tracking-tight text-white sm:text-4xl'>
-                  Streamline Your Real Estate Workflow.
-                  <br />
-                  Start using our app today.
-                </h2>
-                <p className='mx-auto mt-6 max-w-xl text-lg leading-8 text-blue-200'>
-                  Save hours in your week by trying our platform. Made and built
-                  for real-estate professionals.
-                </p>
-                <div className='mt-10 flex items-center justify-center gap-x-6'>
-                  <Link
-                    href='/login'
-                    className='rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-blue-600 shadow-sm hover:bg-blue-50 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white'>
-                    Get started
-                  </Link>
-                  <Link
-                    href='/'
-                    className='text-sm font-semibold leading-6 text-white'>
-                    Learn more <span aria-hidden='true'>→</span>
-                  </Link>
-                </div>
-              </div>
-            </div>
-          </div> */}
-
-          {/* Blog section */}
-          <section className='md:mt-12 h-full w-full mt-8 px-4 '>
-            <BlogSection posts={posts} />
-          </section>
-          <section className='w-full flex justify-center mt-12 px-6 border-t-2 pt-6'>
-            <div className='max-w-7xl w-full'>
-              <div className='w-full'>
-                <h3 className='uppercase text-3xl mb-8 '>Related Links</h3>
-                <ul className='flex flex-col gap-8'>
-                  {links.map((link) => (
-                    <li
-                      id={link.id}
-                      className='underline font-semibold hover:opacity-75'>
-                      <Link href={link.href}>{link.title}</Link>
-                    </li>
-                  ))}
-                </ul>
-                {/* Add your content or components related to "Related Links" here */}
-              </div>
-            </div>
-          </section>
         </main>
       </div>
     </>
@@ -240,6 +129,7 @@ AboutPage.getLayout = function getLayout(page) {
     </DefaultLayout>
   )
 }
+
 export async function getServerSideProps() {
   const posts = await client.fetch(groq`
       *[_type == "post" && publishedAt < now()] | order(publishedAt desc)
@@ -272,39 +162,46 @@ const links = [
 
 const timeline = [
   {
+    name: 'Proof of Concept',
+    description:
+      'In March 2024, the idea came to revolutionize the way real-estate development is completed. The team reached out to domain experts to validate industry pain points, and identified bottlenecks in the development process.',
+    date: 'March 2024',
+    dateTime: '2024-03',
+  },
+  {
     name: 'Ignition',
     description:
-      'In August 2023, a group of passionate real estate and tech enthusiasts came together to address the challenges faced by professionals in accessing municipal real-estate development information. Recognizing a gap in the market, they founded Region Intelligence with the aim to create an aggregated platform for this crucial data.',
-    date: 'Aug 2023',
-    dateTime: '2023-08',
+      'In April 2024, the team began working on a minimum viable product to share with target customers. Aiming to bring value from the beginning.',
+    date: 'April 2024',
+    dateTime: '2024-04',
   },
   {
-    name: 'Conceptual Thinking',
+    name: 'Beta Begins',
     description:
-      'By September, the team had already conceptualized a dynamic platform that could gather, analyze, and present municipal development data. Leveraging their combined expertise, they quickly developed a proof of concept, laying the foundation for what would become a game-changing tool in the real estate and urban planning sector.',
-    date: 'Sept 2023',
-    dateTime: '2023-9',
+      'Working with domain experts, our plan is to make rapid advancements as the team took new ideas from the initial proof of concept and turned it into a MVP. With aim to offer enhanced data analytics capabilities, user-friendly interfaces, and robust integration with various sources. ',
+    date: 'May 2024',
+    dateTime: '2024-05',
   },
   {
-    name: 'Beta begins',
+    name: 'Iterations',
     description:
-      'October witnessed rapid advancements as the team took feedback from the initial proof of concept and channeled it into the development of a beta version. This version offered enhanced data analytics capabilities, user-friendly interfaces, and robust integration with multiple municipal data sources.',
-    date: 'Oct 2023',
-    dateTime: '2023-10',
+      'The team aims to iterate over the product and continue to meet customer needs & expectations. By this time we plan to have expanded to more cities and produce more features according to our KPI metrics. ',
+    date: 'June 2024',
+    dateTime: '2023-06',
   },
   {
-    name: 'Beta Testing',
+    name: 'Product Market Fit',
     description:
-      'In November, the plan is to initiate a closed beta test, inviting industry professionals, city planners, and real estate firms to try out the platform. This period was marked by rigorous testing, feedback collection, and iterative improvements, ensuring that the platform met the highest standards of accuracy and usability.',
-    date: 'Nov 2023',
-    dateTime: '2023-11',
+      'Come July, Region Intelligence is planning to officially launch its platform to our main customers. With a refined financial model, Region Intelligence by now will have a plan to manage the burn rate. ',
+    date: 'July 2024',
+    dateTime: '2024-07',
   },
   {
     name: 'Launch of Version 1',
     description:
-      'Come January,  Region Intelligence is planning to officially launch its platform to the public. Offering unparalleled insights into municipal real-estate development, the platform quickly gained recognition as an indispensable tool for industry professionals, providing them with comprehensive, reliable, and easily accessible data.',
-    date: 'Dec 2023',
-    dateTime: '2023-12',
+      'Come August, Region Intelligence is planning to officially launch its platform to the public. Offering unparalleled insights into the world of real estate development. Stay Tuned!',
+    date: 'August 2024',
+    dateTime: '2024-08',
   },
 ]
 
@@ -330,25 +227,15 @@ const mission = [
     id: 0,
     title: 'Our principles',
     body1:
-      'Region Intelligence is at the forefront of facilitating growth and ease in urban environments by unifying city data into a single, accessible platform. Designed as a powerful big data analytics tool for real estate professionals, it enables users to conduct efficient, thorough, and insightful due diligence. The integration of this diverse city data not only streamlines the decision-making process but also opens up new opportunities for urban development and growth.',
+      'This company was founded on the idea that information should be accessible to everyone. In a future where real estate development is AI-Driven, this company will be at the forefront of providing a wealth of information for real estate professionals of all backgrounds.',
     body2:
-      'With its proprietary artificial intelligence technology, Region Intelligence assures users of reliable, data-driven decisions, paving the way for a new era of real estate professionals. This approach is geared towards reducing time spent on research, thus accelerating the transition from planning to actual development, ultimately leading to more efficient and effective city growth and development. ',
-  },
-  {
-    id: 1,
-    title: 'Founder story',
-    body1:
-      'Founded in 2023, Region Intelligence began its journey in Los Angeles, California. Known as the major leagues of land development, three UC-Irvine graduates embarked on a journey to bring tech into this industry.',
-    body2:
-      'Their focus is mainly for real estate professionals, however the founder’s realized the power of data is beyond real estate itself. Bringing Region Intelligence as the main source for users looking into local government municipalities.',
-  },
-  {
-    id: 2,
-    title: 'Culture',
-    body1:
-      'Region Intelligence strives to create a culture that fosters creativity, collaboration, and commitment. We are passionate about all of our customers and believe that creativity and collaboration are powerful tools to help make their lives easier.',
-    body2:
-      'What makes us different makes us stronger. Actively listening to our user’s needs allows our teams the freedom to be creative in problem solving. No matter what background or community you are from, we actively want to build a more diverse, inclusive, and equitable organization that brings unique perspectives.',
+      'The due-diligence process for land valuation will be an entirely new process that encompasses key variables to give our users the best informed decision-making. ',
+    body3:
+      'Determining optimal solutions for land development will transform how real-estate transactions are completed. With our company being the backbone of the research. Taking the guessing out of brokered deals, land development, and lead generation. ',
+    body4:
+      'Region Intelligence aims to save the time, money, and headache involved in the entire acquisition process. Shedding light on what the future of real estate development can be. ',
+    body5:
+      'Being based in Los Angeles means that we are at the forefront of real estate development in California. With thousands of building permits submitted across over 200 cities. Southern California provides Region Intelligence the best foothold to reach developers of all types. ',
   },
 ]
 
@@ -380,7 +267,7 @@ const people = [
   /* Staff section */
 }
 {
-  /* <div className='bg-white py-12 mt-24 md:my-24'>
+  /* <div className='bg-[#FDFBF5] py-12 mt-24 md:my-24'>
             <div className='mx-auto grid max-w-7xl grid-cols-1 gap-x-8 gap-y-20 px-6 lg:px-8 xl:grid-cols-5'>
               <div className='max-w-2xl xl:col-span-2'>
                 <h2 className='text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl'>
