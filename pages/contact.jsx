f3a425b4a5fda35ae3930edbe43b9e334e324ff5
Darius Garcia <dariusgarcia888@gmail.com>
2024-05-06 17:18:50 -0700

    styling

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -125,7 +125,9 @@ export default function ContactPage() {
             method='POST'
             className='px-6 pb-24 pt-20 sm:pb-32 lg:px-8 lg:py-12'>
             <div className='mx-auto max-w-xl lg:mr-0 lg:max-w-lg'>
-                <div className='mb-12'><img src='logos/logo3.svg' alt='logo' className='w-36'/></div>
+              <div className='mb-12'>
+                <img src='logos/logo3.svg' alt='logo' className='w-36' />
+              </div>
               <div className='grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2'>
                 <div>
                   <label
@@ -211,7 +213,7 @@ export default function ContactPage() {
               <div className='mt-8 flex justify-end'>
                 <button
                   type='submit'
-                  className='rounded-md bg-blue-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600'>
+                  className='rounded-md bg-blue-600 px-3.5 py-2.5 w-full text-center text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600'>
                   Send message
                 </button>
               </div>
